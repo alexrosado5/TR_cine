55eae18054b10b8d9cc1df56a6032f6fcd8e7189
alexrosado5 <18rosadoale@ipm.cat>
2024-03-10 21:40:47 +0100

    Prova 1

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
 # Importar llibreries
 import streamlit as st
 
+# Pàgina principal
+
 # Títol webpage
 st.title("Benvinguts al cinema de l'Àlex")
 st.write("Cartellera")
+
+# Foto peli
+
+st.header("Oppenheimer")
+
+image = "../assets/opp.jpeg"
+st.image(image, width = 300)
+
