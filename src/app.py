8b829e2793cdc0e9da7f3acdfaf10229856aa33d
alexrosado5 <18rosadoale@ipm.cat>
2024-03-25 17:01:39 +0100

    src update

--- src/app.py
+++ src/app.py
@@ -2,9 +2,6 @@
 import streamlit as st
 
 
-
-
-
 def homepage():
     # Pàgina principal
 
@@ -38,11 +35,47 @@ def homepage():
         st.header("Barbie")
         image2 = "../assets/bar.jpeg"
         st.image(image2, width = 235)
-        st.button("Seients", key = "Botó 3")
-        
+        st.button("17:45", key = "Botó 3")
+       
+       
 #Subpagina Openheimer(x1)
 def openheimer():
-    st.write("Hello")
+    st.title("Escull els teus seients")
+    image3 = "../assets/Pantalla cine.png"
+    st.image(image3, width= 800)
+    col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
+    with col1:
+        for i in range(1, 0):
+            st.button(f"A{i}")
+
+    with col2:
+        for i in range(1, 11):
+            st.button(f"A{i}")
+
+    with col3:
+        for i in range(1, 11):
+            st.button(f"B{i}")
+
+    with col4:
+        for i in range(1, 0):
+            st.button(f"D{i}")
+    with col5:
+        for i in range(1, 0):
+            st.button(f"A{i}")
+
+    with col6:
+        for i in range(1, 0):
+            st.button(f"D{i}")
+
+    with col7:
+        for i in range(1, 11):
+            st.button(f"D{i}")
+    with col8:
+        for i in range(1, 11):
+            st.button(f"E{i}")
+
+                
+        
 
 #Subpagina Nieve(x2)
 
@@ -61,8 +94,6 @@ def main():
     elif pagina_seleccionada == "Barbie":
         st.write("Si")
 
-
-  
 
 if __name__=="__main__":
     main()
