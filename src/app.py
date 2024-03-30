f8f07fe59d74da682483814a257e334be6f85aa4
alexrosado5 <18rosadoale@ipm.cat>
2024-03-30 17:07:19 +0100

    update

--- src/app.py
+++ src/app.py
@@ -1,12 +1,7 @@
 # Importar llibreries
 import streamlit as st
 
-c = 0
-c = int(c)
-    
-
-def homepage():
-    global c
+def homepage(session_state):
     # Pàgina principal
 
     # Títol webpage
@@ -22,10 +17,10 @@ def homepage():
         st.header("Oppenheimer")
         image = "../assets/opp.jpeg"
         st.image(image, width = 220)
-        button_clicked= st.button("Seients", key="Botó 1")
-        st.write(button_clicked)
-        if button_clicked == True:
-            c +=1
+        if st.button("Seients", key="Botó 1")== True:
+            session_state.counter = 2
+            st.rerun()
+            
         
 
 
@@ -34,14 +29,18 @@ def homepage():
         st.header("Nieve")
         image1 = "../assets/scc.jpeg"
         st.image(image1, width = 235)
-        st.button("Seients", key= "Botó 2")
+        if st.button("Seients", key= "Botó 2") == True:
+            session_state.counter = 3
+            st.rerun()
 
     ##Foto 3
     with col3:
         st.header("Barbie")
         image2 = "../assets/bar.jpeg"
         st.image(image2, width = 235)
-        st.button("17:45", key = "Botó 3")
+        if st.button("17:45", key = "Botó 3") == True:
+            session_state.counter = 4
+            st.rerun()
        
 
 #Subpagina Openheimer(x1)
@@ -83,8 +82,7 @@ def barbie():
     st.image(image4, width= 800)
     col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
     with col1:
-        for i in range(1, 0):
-            st.button(f"A{i}")
+        pass
 
     with col2:
         for i in range(0, 10):
@@ -95,15 +93,12 @@ def barbie():
             st.button(f"B{i}")
 
     with col4:
-        for i in range(1, 0):
-            st.button(f"D{i}")
+        pass
     with col5:
-        for i in range(1, 0):
-            st.button(f"A{i}")
+        pass
 
     with col6:
-        for i in range(1, 0):
-            st.button(f"D{i}")
+        pass
 
     with col7:
         for i in range(0, 10):
@@ -119,18 +114,22 @@ def barbie():
 
 #Funció principal de la pàgina
 def main():
-    global c
-    paginas = ["Inici", "Oppenheimer", "Barbie", "Nieve"]
-    pagina_seleccionada = st.sidebar.radio("Selecciona la pàgina", paginas)
-    if (pagina_seleccionada =="Inici"):
-        homepage()
     
-    elif (pagina_seleccionada == "Oppenheimer" or c==1):
+    session_state = st.session_state
+    if "counter" not in session_state:
+        session_state.counter = 1
+
+    paginas = ["Inici", "Oppenheimer", "Nieve", "Barbie"]
+    pagina_seleccionada = st.sidebar.radio("Selecciona la pàgina", paginas, index=session_state.counter -1)
+    if (pagina_seleccionada =="Inici"):
+        homepage(session_state)
+    elif (pagina_seleccionada == "Oppenheimer"):
         openheimer()
     elif pagina_seleccionada == "Nieve":
         st.write("No")
     elif pagina_seleccionada == "Barbie":
         barbie()
+    
 
 
 if __name__=="__main__":
